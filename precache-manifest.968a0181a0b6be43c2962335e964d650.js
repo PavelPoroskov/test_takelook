90791364d8e993bb2d0d6b00f721a0494c7c0281
PavelPoroskov <pavl1853@gmail.com>
2019-07-20 04:22:03 +0300

    Updates

---
+++ precache-manifest.968a0181a0b6be43c2962335e964d650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2f3e44fd6f6b27a25371134c5b3f78",
+    "url": "/test_takelook/index.html"
+  },
+  {
+    "revision": "cfd30e6c86a76aaa0ddd",
+    "url": "/test_takelook/static/css/2.64e3a141.chunk.css"
+  },
+  {
+    "revision": "8fe13d9420605eed7ce8",
+    "url": "/test_takelook/static/css/main.267b598a.chunk.css"
+  },
+  {
+    "revision": "cfd30e6c86a76aaa0ddd",
+    "url": "/test_takelook/static/js/2.4966bbfc.chunk.js"
+  },
+  {
+    "revision": "8fe13d9420605eed7ce8",
+    "url": "/test_takelook/static/js/main.dcf2b3fa.chunk.js"
+  },
+  {
+    "revision": "4a5df07eb9cf1b87ab98",
+    "url": "/test_takelook/static/js/runtime~main.c3125151.js"
+  }
+]);
